0d1fadd351b9778ff1d849039a189c56b792e056
Andy5847 <49703077+Andy5847@users.noreply.github.com>
2020-05-06 21:10:51 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,34 +1,49 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-import numpy as np
+from functions.weighted_graph_drawings import draw_subtree
+from functions.prims_functions import cost, min_prims_edges
+
+G = nx.read_weighted_edgelist('data/G3.txt', nodetype = int)
 
 
-G = nx.read_weighted_edgelist('data/G1.txt', nodetype = int)
-
-def show_weighted_graph(G):
-    pos = nx.planar_layout(G)
-    nx.draw(G, pos)
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G,
-                                 pos,
-                                 edge_labels = labels)
-    plt.show()
+def prims_algorithm(G, starting_node, draw = False, attrib = False):
     
-def draw_subtree(G, T):
-    pos = nx.planar_layout(G)
-    nx.draw_networkx(G, pos)
-    labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G,
-                                 pos,
-                                 edge_labels = labels,)
-    nx.draw_networkx_edges(G, pos,
-                            edgelist=T.edges(),
-                            width=8, alpha=0.5,
-                            edge_color='r',)
-    nx.draw_networkx_nodes(G,
-                           pos,
-                           nodelist=T.nodes(),
-                           node_color='r',
-                           node_size=500,
-                           alpha=0.8)
-    plt.show()
+    T= nx.Graph()
+    T.add_node(starting_node)
+    
+    if draw == True:
+        draw_subtree(G, T)
+        
+    while set(T.nodes()) != set(G.nodes()):
+        e = min_prims_edges(G, T)
+        T.add_edge(e[0], e[1], weight = cost(G, e))
+        if draw == True:
+            draw_subtree(G, T)
+            
+    if attrib == True:
+        total_cost = sum(cost(G, e) for e in T.edges())
+        print('')
+        print('------------PROPERTIES OF THE TREE T----------------')
+        print('----------------------------------------------------')
+        print(f'V(T) = {list(T.nodes())}')
+        print(f'E(T) = {list(T.edges())}')
+        print(f'Total Cost = {total_cost}')
+        print('----------------------------------------------------')
+        
+    return T
+
+
+                              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
